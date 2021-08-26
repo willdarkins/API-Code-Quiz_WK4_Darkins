5ab7b834844cf80f8d61f9e87f3c4bb71ace30c9
Will Darkins <willdarkins@gmail.com>
2021-08-26 08:37:02 -0700

    basic timer function created

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+// Basic Timer function set for 1:00 to complete the quiz
+var endGame = function() {
+    alert('You have run out of time');
+};
+
+var startCountdown = setTimeout(endGame,60000)
