121bc16059bdf2b363cca84a18e5da9f26ae5b70
Will Darkins <willdarkins@gmail.com>
2021-09-03 10:16:30 -0700

    final comments and adjustments made to code, with example video

--- assets/js/secondary_script.js
+++ assets/js/secondary_script.js
@@ -1,3 +1,4 @@
+// Global Variables
 var goBackButton = document.getElementById('go-back');
 goBackButton.innerHTML = "Go Back";
 var clearHighScoresButton = document.getElementById('clear-high-scores');
